b91d9ad8b141c72d2cbb77bfa31685ac725217f6
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-05-10 14:21:51 +0530

    feat: set invoice value & other details of e-invoice

--- erpnext_e_invoicing/erpnext_e_invoicing/doctype/e_invoice/e_invoice.py
+++ erpnext_e_invoicing/erpnext_e_invoicing/doctype/e_invoice/e_invoice.py
@@ -3,9 +3,13 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-import frappe
+
 from json import loads, dumps
+
+import frappe
+from frappe import _
 from frappe.model.document import Document
+from frappe.utils.data import cint, format_date
 
 from erpnext.regional.india.utils import get_gst_accounts
 
@@ -22,6 +26,10 @@ class EInvoice(Document):
 		self.set_buyer_details()
 		self.set_shipping_details()
 		self.set_item_details()
+		self.set_value_details()
+		self.set_payment_details()
+		self.set_return_doc_reference()
+		self.set_eway_bill_details()
 
 	def set_sales_invoice(self):
 		self.sales_invoice = frappe.get_doc('Sales Invoice', self.invoice)
@@ -100,8 +108,15 @@ class EInvoice(Document):
 				self.shipping_pincode = 999999
 				self.shipping_place_of_supply = 96
 
+	def get_invoice_discount_type(self):
+		if self.sales_invoice.discount_amount:
+			return self.sales_invoice.apply_discount_on
+
+		return ''
+
 	def set_item_details(self):
-		discount_applied_on_net_total = self.sales_invoice.apply_discount_on == 'Net Total' and self.sales_invoice.discount_amount
+		self.items = []
+		discount_applied_on_net_total = self.get_invoice_discount_type() == 'Net Total'
 
 		for item in self.sales_invoice.items:
 			is_service_item = frappe.db.get_value('Item', item.item_code, 'is_stock_item')
@@ -163,4 +178,103 @@ class EInvoice(Document):
 			else:
 				# TODO: other charges per item
 				pass
+
+	def set_value_details(self):
+		invoice_value_details = frappe._dict(dict())
+
+		if self.get_invoice_discount_type() == 'Net Total':
+			# Discount already applied on net total which means on items
+			self.ass_value = abs(sum([i.taxable_value for i in self.get('items')]))
+			self.invoice_discount = 0
+		elif self.get_invoice_discount_type() == 'Grand Total':
+			self.ass_value = abs(sum([i.taxable_value for i in self.get('items')]))
+			self.invoice_discount = self.sales_invoice.base_discount_amount
+		else:
+			# no invoice level discount applied
+			self.ass_value = abs(sum([i.taxable_value for i in self.get('items')]))
+			self.invoice_discount = 0
+
+		self.round_off_amount = self.sales_invoice.base_rounding_adjustment
+		self.base_invoice_value = abs(self.sales_invoice.base_rounded_total) or abs(self.sales_invoice.base_grand_total)
+		self.invoice_value = abs(self.sales_invoice.rounded_total) or abs(self.sales_invoice.grand_total)
+
+		self.set_invoice_tax_details()
+
+	def set_invoice_tax_details(self):
+		gst_accounts = get_gst_accounts(self.company)
+		gst_accounts_list = [d for accounts in gst_accounts.values() for d in accounts if d]
+
+		self.cgst_value = 0
+		self.sgst_value = 0
+		self.igst_value = 0
+		self.cess_value = 0
+		self.other_charges = 0
+		considered_rows = []
+
+		for t in self.sales_invoice.taxes:
+			tax_amount = t.base_tax_amount if self.get_invoice_discount_type() == 'Grand Total' \
+							else t.base_tax_amount_after_discount_amount
+
+			if t.account_head in gst_accounts_list:
+				if t.account_head in gst_accounts.cess_account:
+					# using after discount amt since item also uses after discount amt for cess calc
+					self.cess_value += abs(t.base_tax_amount_after_discount_amount)
+
+				for tax in ['igst', 'cgst', 'sgst']:
+					if t.account_head in gst_accounts[f'{tax}_account']:
+						new_value = self.get(f'{tax}_value') + abs(tax_amount)
+						self.set(f'{tax}_value', new_value)
+
+					self.update_other_charges(t, gst_accounts_list, considered_rows)
+			else:
+				self.other_charges += abs(tax_amount)
+	
+	def update_other_charges(self, tax_row, gst_accounts_list, considered_rows):
+		taxes = self.sales_invoice.get('taxes')
+		prev_row_id = cint(tax_row.row_id) - 1
+
+		if tax_row.account_head in gst_accounts_list and prev_row_id not in considered_rows:
+			if tax_row.charge_type == 'On Previous Row Amount':
+				amount = taxes[prev_row_id].tax_amount_after_discount_amount
+				self.other_charges -= abs(amount)
+				considered_rows.append(prev_row_id)
+			if tax_row.charge_type == 'On Previous Row Total':
+				amount = taxes[prev_row_id].base_total - self.sales_invoice.base_net_total
+				self.other_charges -= abs(amount)
+				considered_rows.append(prev_row_id)
+
+	def set_payment_details(self):
+		if self.sales_invoice.is_pos and self.sales_invoice.base_paid_amount:
+			self.payee_name = self.company
+			self.mode = ', '.join([d.mode_of_payment for d in self.sales_invoice.payments if d.amount > 0])
+			self.paid_amount = self.sales_invoice.base_paid_amount
+			self.outstanding_amount = self.sales_invoice.outstanding_amount
+
+	def set_return_doc_reference(self):
+		if self.sales_invoice.is_return:
+			if not self.sales_invoice.return_against:
+				frappe.throw(_('For generating IRN, reference to the original invoice is mandatory for a credit note. Please set {} field to generate e-invoice.')
+					.format(frappe.bold('Return Against')), title=_('Missing Field'))
+
+			self.previous_document_no = self.sales_invoice.return_against
+			original_invoice_date = frappe.db.get_value('Sales Invoice', self.sales_invoice.return_against, 'posting_date')
+			self.previous_document_date = format_date(original_invoice_date, 'dd/mm/yyyy')
+
+	def set_eway_bill_details(self):
+		if self.sales_invoice.transporter:
+			if self.sales_invoice.is_return:
+				frappe.throw(_('E-Way Bill cannot be generated for Credit Notes & Debit Notes. Please clear fields in the Transporter Section of the invoice.'),
+					title=_('Invalid Fields'))
+
+			mode_of_transport = {'Road': '1', 'Air': '2', 'Rail': '3', 'Ship': '4'}
+			vehicle_type = {'': '', 'Regular': 'R', 'Over Dimensional Cargo (ODC)': 'O'}
+
+			self.tranporter_gstin = self.sales_invoice.gst_transporter_id,
+			self.transporter_name = self.sales_invoice.transporter_name,
+			self.mode_of_transport = mode_of_transport[self.sales_invoice.mode_of_transport],
+			self.distance = self.sales_invoice.distance or 0,
+			self.transport_document_no = self.sales_invoice.lr_no,
+			self.transport_document_date = format_date(self.sales_invoice.lr_date, 'dd/mm/yyyy'),
+			self.vehicle_no = self.sales_invoice.vehicle_no,
+			self.vehicle_type = vehicle_type[self.sales_invoice.gst_vehicle_type]
 
