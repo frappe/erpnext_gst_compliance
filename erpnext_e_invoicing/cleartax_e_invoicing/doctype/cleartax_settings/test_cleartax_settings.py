f3532ffe89c2e721e6763fe161056e7adf8c614e
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-05-12 12:46:01 +0530

    feat: initialize test for cleartax settings

--- erpnext_e_invoicing/cleartax_e_invoicing/doctype/cleartax_settings/test_cleartax_settings.py
+++ erpnext_e_invoicing/cleartax_e_invoicing/doctype/cleartax_settings/test_cleartax_settings.py
@@ -3,8 +3,19 @@
 # See license.txt
 from __future__ import unicode_literals
 
-# import frappe
+import frappe
 import unittest
 
 class TestCleartaxSettings(unittest.TestCase):
-	pass
+	
+	def test_incorrect_company_gstin(self):
+		cleartax_settings = frappe.get_doc('Cleartax Settings')
+		cleartax_settings.enabled = 1
+
+		cleartax_settings.append('credentials', {
+			'company': '_Test Company',
+			'gstin': '27AAFCD5862R013',
+			'owner_id': 'test_owner_id'
+		})
+
+		self.assertRaises(frappe.ValidationError, cleartax_settings.save)
