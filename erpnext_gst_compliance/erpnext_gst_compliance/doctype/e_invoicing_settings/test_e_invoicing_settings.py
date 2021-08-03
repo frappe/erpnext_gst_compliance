37e1dc85fce1b28f4a462955078aea4da59a9a63
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-08-03 19:01:37 +0530

    test: e invoicing settings

--- erpnext_gst_compliance/erpnext_gst_compliance/doctype/e_invoicing_settings/test_e_invoicing_settings.py
+++ erpnext_gst_compliance/erpnext_gst_compliance/doctype/e_invoicing_settings/test_e_invoicing_settings.py
@@ -5,8 +5,26 @@ import frappe
 import unittest
 
 class TestEInvoicingSettings(unittest.TestCase):
-
 	def test_service_provider_is_disabled(self):
 		e_invoicing_settings = frappe.get_doc('E Invoicing Settings')
-		e_invoicing_settings.service_provider = 'Cleartax Settings'
+		e_invoicing_settings.service_provider = 'Adequare Settings'
 		self.assertRaises(frappe.ValidationError, e_invoicing_settings.save)
+
+		adequare_settings = frappe.get_single('Adequare Settings')
+		adequare_settings.enabled = 1
+		adequare_settings.flags.ignore_validate = True
+		adequare_settings.save()
+
+		e_invoicing_settings.reload()
+		e_invoicing_settings.service_provider = 'Adequare Settings'
+		e_invoicing_settings.save()
+
+		adequare_settings.reload()
+		adequare_settings.enabled = 0
+		adequare_settings.flags.ignore_validate = True
+		adequare_settings.save()
+
+		e_invoicing_settings.reload()
+		e_invoicing_settings.service_provider = None
+		e_invoicing_settings.flags.ignore_mandatory = True
+		e_invoicing_settings.save()
