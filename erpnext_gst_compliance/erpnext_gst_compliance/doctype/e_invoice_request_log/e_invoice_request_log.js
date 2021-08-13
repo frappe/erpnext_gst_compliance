d82b67c1a6f1dc5c1c6435507944ebb3c89ab4b7
Saqib Ansari <nextchamp.saqib@gmail.com>
2021-08-13 13:09:40 +0530

    feat: e invoice request log doctype

---
+++ erpnext_gst_compliance/erpnext_gst_compliance/doctype/e_invoice_request_log/e_invoice_request_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Frappe Technologied Pvt. Ltd. and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('E Invoice Request Log', {
+	// refresh: function(frm) {
+
+	// }
+});
